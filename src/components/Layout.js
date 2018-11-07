ad4c432329c25f151b4c3d3a4636ec6644ee030f
davieu <davisumana@gmail.com>
2018-11-07 18:56:45 -0500

    initial commit added css & cleared out unwanted create-react-app files & added Layout component & made socket server onnection

---
+++ src/components/Layout.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client'
+
+const socketUrl = "http://10.0.0.13:3231"
+export default class Layout extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            socket:null
+        };
+    };
+
+    componentWillMount() {
+        this.initSocket();
+    };
+
+    initSocket = () => {
+        const socket = io(socketUrl)
+        socket.on('connect', () => {
+            console.log("Connected", socket.id);
+        })
+        this.setState({socket})
+    };
+
+    render() {
+        const { title } = this.props
+        return (
+            <div className="container">
+                {title}
+            </div>
+        );
+    };
+};
